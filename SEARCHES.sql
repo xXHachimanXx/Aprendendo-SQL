62c6f560de46af150c0dbfafe1335e31b252d108
xXHachimanXx <globaldep90@gmail.com>
2019-11-18 19:57:30 -0200

    Modificando inserts e fazendo a questão 18

--- SEARCHES.sql
+++ SEARCHES.sql
@@ -38,8 +38,36 @@ select NOME from aluno as a
 select distinct a.NOMEAUT from autor as a, autoria as au where au.CODLIVRO = 
 ( select CODLIVRO from autoria where codautor = (select CODIGO from autor where NOMEAUT = "NAVATHE") ) and a.codigo = au.codautor and a.nomeaut <> "NAVATHE";
 
+-- questao 15	
+select titulo, max(count(*)) from livro as l
+join exemplar as ex on ex.codlivro = l.codigo
+join itememprest as ie on ie.codexemplar = ex.codexemplar
+join emprestimo as emp on emp.codemprest = ie.codemprestitem ;
+
+-- questao 16 valor exemplar > this.edit.avg(valorexemplar)
+-- select l.titulo, ex.codexemplar from (exemplar as ex
+-- join livro as l on l.codigo = ex.codlivro
+-- join editora as ed on l.codedit = ed.codigo)
+-- where ;
+
+select avg(valoraquisicao) from exemplar as ex
+join livro as l on l.codigo = ex.codlivro
+join editora as ed on l.codedit = ed.codigo;
+
+select l.titulo, ex.codexemplar from livro as l, editora as ed, exemplar as ex
+where ex.valorexemplar > (select avg(valorexemplar) from editora where l.codedit = codigo and ex.codlivro = l.codigo);
+
+select avg(valoraquisicao) from exemplar as e where e.codlivro = (select codigo from livro)
+
+-- e.valorexemplar > avg(select valorexemplar from exemplar where ed.cod = l.codeditora);
+-- questao 17
+select distinct codigo from aluno as a, emprestimo as e where e.codaluno <> a.codigo;
 
 
+-- questao 18
+update livro as l
+set l.codedit = (select codigo from editora where nomeedit = "PEARSON")
+where l.codedit = (select codigo from editora where nomeedit = "MAKRON BOOKS");
 
 
     
